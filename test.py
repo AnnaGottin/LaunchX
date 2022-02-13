6795c52b2664dc9e877d99fc72189aa7ea0d2910
AnnaGottin <shabyanna@gmail.com>
2022-02-13 15:58:25 -0600

    Ciclos
    Kata Modulo 7

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Neptune']
-planet = input( "Inserta planeta:" )
-planetX = planets.index(planet)
-if(planetX > -1):
-    print("El indice del planeta es: ", planetX)
-    print(f"Los planetas mas cercanos desde {planet} son: ", planets[:planetX] )
-    print("Los planetas mas lejanos desde {planet} son: ", planets[planetX+1:] )
-else:
-    print("Planeta no encontrado")
+new_planet = ''
+planets = []
+while new_planet.lower() != "done":
+    if new_planet:
+        planets.append(new_planet)
+    new_planet = input("Proporcione el planeta, sino escriba 'done' ")
+
+for planet in planets:
+    print(planet)
