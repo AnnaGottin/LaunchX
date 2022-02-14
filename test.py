8582b2c393f703bc1701e0da7c71fa1f57524596
AnnaGottin <shabyanna@gmail.com>
2022-02-13 22:27:42 -0600

    Funciones
    Kata Modulo 9

--- test.py
+++ test.py
@@ -1,9 +1,20 @@
-new_planet = ''
-planets = []
-while new_planet.lower() != "done":
-    if new_planet:
-        planets.append(new_planet)
-    new_planet = input("Proporcione el planeta, sino escriba 'done' ")
+def promedio(t1, t2, t3):
+    return (t1+t2+t3)/3
 
-for planet in planets:
-    print(planet)
+def informes(t1, t2, t3):
+    return f""" Promedio: {promedio(t1,t2,t3) }
+    Lectura Tanque 1: {t1}
+    Lectura Tanque 2: {t2}
+    Lectura Tanque 3: {t3}
+    """
+
+print(informes(5,8,10))
+
+def informe(destino, *time  ,**fuel_reservoirs):
+    return f"""
+    Destino: {destino}
+    Tiempo de vuelo: {sum(time)}
+    Total de combustible: {sum(fuel_reservoirs.values() )}
+    """
+
+print(informe('Moon', 1,2,3, t1=10,t2=15,t3=20   )  )
