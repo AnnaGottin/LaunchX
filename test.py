c6d5cba2e01f50a1b78a849b4596724fc5dc8925
AnnaGottin <shabyanna@gmail.com>
2022-02-13 23:13:46 -0600

    Errores
    Kata Modulo 10

--- test.py
+++ test.py
@@ -1,20 +1,61 @@
-def promedio(t1, t2, t3):
-    return (t1+t2+t3)/3
+def main():
+    open("/path/to/mars.jpg")
 
-def informes(t1, t2, t3):
-    return f""" Promedio: {promedio(t1,t2,t3) }
-    Lectura Tanque 1: {t1}
-    Lectura Tanque 2: {t2}
-    Lectura Tanque 3: {t3}
-    """
+if __name__ == '__main__':
+    main()
 
-print(informes(5,8,10))
 
-def informe(destino, *time  ,**fuel_reservoirs):
-    return f"""
-    Destino: {destino}
-    Tiempo de vuelo: {sum(time)}
-    Total de combustible: {sum(fuel_reservoirs.values() )}
-    """
+try:
+    open('config.txt')
+except FileNotFoundError:
+    print("Couldn't find the config.txt file!")
 
-print(informe('Moon', 1,2,3, t1=10,t2=15,t3=20   )  )
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
+    except (BlockingIOError, TimeoutError):
+        print("Filesystem under heavy load, can't complete reading configuration file")
+
+
+if __name__ == '__main__':
+    main()
+
+def main():
+    try:
+        open("mars.jpg")
+    except FileNotFoundError as err:
+        print("got a problem trying to read the file:", err)
+
+    try:
+        open("config.txt")
+    except OSError as err:
+        if err.errno == 2:
+            print("Couldn't find the config.txt file!")
+        elif err.errno == 13:
+            print("Found config.txt but couldn't read it")
+
+if __name__ == '__main__':
+    main()
+
+def water_left(astronauts, water_left, days_left):
+    for argument in [astronauts, water_left, days_left]:
+        try:
+            # If argument is an int, the following operation will work
+            argument / 10
+        except TypeError:
+            # TypError will be raised only if it isn't the right type 
+            # Raise the same exception but with a better error message
+            raise TypeError(f"All arguments must be of type int, but received: '{argument}'")
+    daily_usage = astronauts * 11
+    total_usage = daily_usage * days_left
+    total_water_left = water_left - total_usage
+    if total_water_left < 0:
+        raise RuntimeError(f"There is not enough water for {astronauts} astronauts after {days_left} days!")
+    return f"Total water left after {days_left} days is: {total_water_left} liters"
+
+print(water_left("3", "200", None))
